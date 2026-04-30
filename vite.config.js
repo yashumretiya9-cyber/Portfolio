e99fc20ef15847af3bd8a85b75cd06d90442070e
Yash <yashumretiya9@gmail.com>
2026-04-30 20:26:39 +0530

    Portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Portfolio/",
-})
+  base: "/yashumretiya9-cyber/",
+  // base: "/Portfolio/",
+});
